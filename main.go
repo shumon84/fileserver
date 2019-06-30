92004633d6623219f21e2a90fd30524d90620278
shumon84 <cameremon84@gmail.com>
2019-06-30 23:15:10 +0900

    Add basic auth option

--- main.go
+++ main.go
@@ -2,25 +2,52 @@ package main
 
 import (
 	"flag"
+	"io"
+	"log"
 	"net/http"
+	"strings"
 )
+
+func BasicAuthMiddleWare(user string, password string, handler http.Handler) http.Handler {
+	if user == "" && password == "" {
+		return handler
+	}
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		reqUser, reqPassword, ok := r.BasicAuth()
+		if ok && reqUser == user && reqPassword == password {
+			handler.ServeHTTP(w, r)
+			return
+		}
+		w.WriteHeader(http.StatusUnauthorized)
+		io.WriteString(w, http.StatusText(http.StatusUnauthorized))
+	})
+}
 
 func main() {
 	var (
-		path string
-		port string
+		path         string
+		port         string
+		basicAuthKey string
 	)
 
 	flag.StringVar(&path, "d", ".", "ファイルサーバのルートディレクトリ")
 	flag.StringVar(&port, "p", "8080", "割り当てるポート")
+	flag.StringVar(&basicAuthKey, "b", ":", "Basic認証を使用する[user:password]")
 	flag.Parse()
+
+	key := strings.Split(basicAuthKey, ":")
+	if len(key) != 2 {
+		flag.Usage()
+		return
+	}
 
 	server := http.Server{
 		Addr: ":" + port,
 	}
 
-	http.Handle("/", http.StripPrefix("/",
-		http.FileServer(http.Dir(path))))
+	http.Handle("/",
+		BasicAuthMiddleWare(key[0], key[1], http.StripPrefix("/", http.FileServer(http.Dir(path)))),
+	)
 
-	server.ListenAndServe()
+	log.Fatal(server.ListenAndServe())
 }
